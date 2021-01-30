59d2c4389498ee6787db84d3ab25c223536623c1
Deniz A. Durmaz <denizalpd@ogr.iu.edu.tr>
2021-01-30 20:48:19 +0300

    Add development nix shell

---
+++ hush.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, ghcid, nixfmt, stdenv, zlib }:
+mkDerivation {
+  pname = "scully";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base ghcid nixfmt zlib ];
+  executableHaskellDepends = [ base ];
+  doHaddock = false;
+  license = stdenv.lib.licenses.bsd3;
+  shellHook = ''
+      hpack
+    '';
+}
